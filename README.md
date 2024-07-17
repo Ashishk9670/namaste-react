bf75d108f64a501b412047354e30637617ed0b29
ASHISH KUMAR <akumar12@student.nitw.ac.in>
2024-07-18 03:50:53 +0530

    Episode-02

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+# Parcel
+- Dev build
+- Local server
+- HMR hot module replacement
+- File watching algorithm -written in C++
+- Cathcing -faster build
+- Image optimisation
+- Minification of file
+- Bundling of files
+- Compress
+- Consistent Hashing
+- Code Splitting
+- Differential bundling -  to support older browsers
+- Diagnostic
+- Error Handling
+- Host on https 
+- Tree shaking - remove ununsed code
+- Differential dev and prod build
--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", {id:"parent"}, [
      React.createElement( "div",{id:"child"}, [
-        React.createElement("h1", {}, "I am h1 tag" ),
+        React.createElement("h1", {}, "I am h1 tag tag" ),
         React.createElement( "h2", {}, "I am h2 tag" )
     ]),
         React.createElement(  "div", {id:"child2"},[
@@ -12,11 +12,6 @@ const parent = React.createElement("div", {id:"parent"}, [
     ])
  ]);
 
-// const heading = React.createElement(
-//     "h1",
-//     {id: "heading"},
-//     "Hello world from react"
-// );
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
