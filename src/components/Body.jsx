3070149e6362f49fe11f733e70552a3cc2ef8aec
ASHISH KUMAR <akumar12@student.nitw.ac.in>
2024-07-19 00:54:53 +0530

    ShimmerUI

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,12 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import Shimmer from "./Shimmer";
+
+
+
 
 const Body = () => {
-    const [listOfRestaurant,setListOfRestaurant] = useState(resList); //state variables
-    return (
+    const [listOfRestaurant,setListOfRestaurant] = useState([]); //state variables
+
+    useEffect(()=>{
+        fetchData();
+    },[]);
+
+    const fetchData = async() => {
+        const data =await fetch(
+            "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        );
+        const json = await data.json();
+
+        // optional chaining
+        // console.log(json);
+        setListOfRestaurant(json.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+    };
+      
+    return listOfRestaurant.length == 0 ? <Shimmer/> :  (
         <div className="body">
             <div className="filter">
                 <button className="filter-btn" onClick={() => {
@@ -31,3 +51,26 @@ const Body = () => {
 }
 
 export default Body;
+
+// const url = 'https://http-cors-proxy.p.rapidapi.com/';
+// const options = {
+// 	method: 'POST',
+// 	headers: {
+// 		'x-rapidapi-key': 'Sign Up for Key',
+// 		'x-rapidapi-host': 'http-cors-proxy.p.rapidapi.com',
+// 		'Content-Type': 'application/json',
+// 		Origin: 'www.example.com',
+// 		'X-Requested-With': 'www.example.com'
+// 	},
+// 	body: {
+// 		url: 'https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.49690&lng=80.32460&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+// 	}
+// };
+
+// try {
+// 	const response = await fetch(url, options);
+// 	const result = await response.text();
+// 	console.log(result);
+// } catch (error) {
+// 	console.error(error);
+// }
